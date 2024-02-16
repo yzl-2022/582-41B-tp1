19d4709f9291138f7e0b3514d7a6879de0151da0
Yu <e2296635@cmaisonneuve.qc.ca>
2024-02-16 14:49:38 -0500

    add index view

--- resources/views/etudiant/index.blade.php
+++ resources/views/etudiant/index.blade.php
@@ -1,5 +1,29 @@
 @extends('layouts.app')
 @section('title', 'Étudiants')
 @section('content')
-
+<section class="py-5 text-center container">
+    <h1>Étudiants</h1>
+</section>
+<div class="album container">
+    <div class="row">
+        @forelse($etudiants as $etudiant)
+        <div class="col-md-4 container">
+            <div class="card shadow-sm mb-4">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $etudiant->nom }}</h5>
+                    <p class="card-text">{{ $etudiant->adresse }}</p>
+                    <p class="card-text">{{ $etudiant->telephone }}</p>
+                    <p class="card-text">{{ $etudiant->email }}</p>
+                    <p class="card-text">{{ $etudiant->date_de_naissance }}</p>
+                    <p class="card-text">{{ $etudiant->ville }}</p>
+                    <a href="#" class="card-link">Card link</a>
+                    <a href="#" class="card-link">Another link</a>
+                </div>
+            </div>
+        </div>
+        @empty
+            <div class="alert alert-danger">Aucun étudiant à afficher!</div>
+        @endforelse
+    </div>
+</div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,9 +36,9 @@
             </div>
         </nav>
     </header>
-    <div class="container">
+    <main class="container">
         @yield('content')
-    </div>
+    </main>
     <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top bg-dark" data-bs-theme="dark">
         <div class="col-md-4 d-flex align-items-center">
             <a href="/" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
